7f11b477fca81ea29076eef43828e66ac51d4ae6
Prithvi Raj Polaki <prithvipolaki@gmail.com>
2017-10-12 01:34:29 +0530

    more functionality added

--- js/controllers/todoController.js
+++ js/controllers/todoController.js
@@ -1,7 +1,48 @@
 app.controller('todoController', ['$scope', 'todoService', '$routeParams', function($scope, todoService, $routeParams){
     todoService.getTodosByUserId($routeParams.id).then(function (response) {
-        $scope.user = response.data;
+        $scope.todos = response.data;
     }, function (error) {
         $scope.error = 'Unable to load user data: ' + error.message;
     });
+
+    $scope.deleteTodo = function(index, id){
+        var confirmDelete = confirm("Are you sure want to delete Todo!");
+        if(confirmDelete) {
+            todoService.deleteTodo(id).then(function (response) {
+                if(response.status && response.status === 200) {
+                    $scope.todos.splice(index, 1);
+                }            
+            }, function (error) {
+                $scope.error = 'Unable to load user data: ' + error.message;
+            });
+        }        
+    };
+    $scope.updateTodo = function(index, id){        
+        todoService.updateTodo(id).then(function (response) {
+            if(response.status && response.status === 200) {
+                console.log('status changed successfully');
+            }            
+        }, function (error) {
+            $scope.error = 'Unable to load user data: ' + error.message;
+        });                
+    };
+
+    $scope.saveTodo = function(){
+        if($scope.addToDo === undefined || $scope.addToDo.trim().length === 0) {
+            return false
+        }
+        var data = {            
+            "title": $scope.addToDo,
+            "completed": false
+        };        
+        todoService.addTodo(data).then(function (response) {
+            if(response.status && response.status === 201) {                
+                $scope.todos.push(response.data);
+                $scope.addToDo = '';
+            }            
+        }, function (error) {
+            $scope.error = 'Unable to load user data: ' + error.message;
+        });                
+    };
+    
 }]);
--- js/services/todoService.js
+++ js/services/todoService.js
@@ -3,6 +3,15 @@ app.factory('todoService', ['$http', function($http){
     return {
         getTodosByUserId: function(id) {
            return $http.get('https://jsonplaceholder.typicode.com/todos?userId=' + id)
+        },
+        deleteTodo: function(id) {
+            return $http.delete('https://jsonplaceholder.typicode.com/todos/' + id);
+        },
+        updateTodo: function(id) {
+            return $http.put('https://jsonplaceholder.typicode.com/todos/' + id);
+        },
+        addTodo: function(data) {
+            return $http.post('https://jsonplaceholder.typicode.com/todos', data);
         }
     }
 }]);
